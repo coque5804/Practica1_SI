b5f42c44382987c4777b4f327c1e5b2592bdbe46
coque5804 <coquemarquezsoria@gmail.com>
2025-03-13 18:38:30 +0100

    Leer datos del archivo y conectar BBDD

--- main.py
+++ main.py
@@ -4,10 +4,36 @@ import json
 
 # Leer datos del archivo JSON
 with open('datos.json', 'r') as file:
-    data = json.load(file)
+    datos = json.load(file)
+
+print(datos)
+print(datos["tickets_emitidos"])
 
 # Conectar a la base de datos SQLite
-conn = sqlite3.connect('incidentes.db')
-cursor = conn.cursor()
+con = sqlite3.connect('datos.db')
+cur = con.cursor()
 
-#master
+# Crear tablas en la base de datos
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Incidentes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cliente TEXT,
+    fecha_apertura TEXT,
+    fecha_cierre TEXT,
+    es_mantenimiento BOOLEAN,
+    satisfaccion_cliente INTEGER,
+    tipo_incidencia INTEGER
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Contactos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    incidente_id INTEGER,
+    id_emp TEXT,
+    fecha TEXT,
+    tiempo REAL,
+    FOREIGN KEY (incidente_id) REFERENCES Incidentes (id)
+)
+''')
+con.commit()
